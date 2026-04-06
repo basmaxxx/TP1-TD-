934963305ee85fc6cf83b0691bbbd62732e5082c
violefinck <violefinck0302@gmail.com>
2026-04-06 17:55:53 -0300

    Update Backtracking.cpp

--- TP1/source/Backtracking.cpp
+++ TP1/source/Backtracking.cpp
@@ -2,33 +2,33 @@
 
 void encontrarSeamBacktrackingRec(const std::vector<std::vector<double>>& energia, int i,int j,int n ,int m ,std::pair<std::vector<int>, double>&S ,std::pair<std::vector<int>, double>&B) {
     if(i==n){
-        if(B.empty() || S[1] < B[1]){
+        if(B.first.empty() || S.second < B.second){
             B=S;
         }
         return;
     }
-    if (S[1] < B[1]){
+    if (S.second < B.second){
         if(j>0){ //bajamos por la izquierda siempre y cuando no estemos en un borde
-        S[0].push_back(j - 1);
-        s[1]+=energia[i][j-1];
-        encontrarSeamFuerzaBrutaRec(energia, i + 1, j - 1, n, m, S, B);
-        S[0].pop_back();
-        s[1]-=energia[i][j-1];
+            S.first.push_back(j - 1);
+            S.second+=energia[i+1][j-1];
+            encontrarSeamBacktrackingRec(energia, i + 1, j - 1, n, m, S, B);
+            S.first.pop_back();
+            S.second-=energia[i+1][j-1];
     }
     
         //en todos los casos vamos a bajar por la rama del medio
-        S[0].push_back(j);
-        s[1]+=energia[i][j];
-        encontrarSeamFuerzaBrutaRec(energia, i + 1, j, n, m, S, B);
-        S[0].pop_back();
-        s[1]-=energia[i][j];
+        S.first.push_back(j);
+        S.second+=energia[i+1][j];
+        encontrarSeamBacktrackingRec(energia, i + 1, j, n, m, S, B);
+        S.first.pop_back();
+        S.second-=energia[i+1][j];
     
         if(j<m-1){ //bajamos por la rama derecha siempre y cuando no estemos en un borde
-            S[0].push_back(j + 1);
-            s[1]+=energia[i][j+1];
-            encontrarSeamFuerzaBrutaRec(energia, i + 1, j + 1, n, m, S, B);
-            S[0].pop_back();
-            s[1]-=energia[i][j+1];
+            S.first.push_back(j + 1);
+            S.second+=energia[i+1][j+1];
+            encontrarSeamBacktrackingRec(energia, i + 1, j + 1, n, m, S, B);
+            S.first.pop_back();
+            S.second-=energia[i+1][j+1];
         }
     }else{
         return;}
@@ -41,8 +41,8 @@ std::vector<int> encontrarSeamBacktracking(const std::vector<std::vector<double>
     std::pair<std::vector<int>,double> B;
     for (int j = 0; j < m; j++) { //hago la funcion recursiva por cada elemento de la primera fila
         std::pair<std::vector<int>,double> S;
-        S[0].push_back(j);
+        S.first.push_back(j);
         encontrarSeamBacktrackingRec(energia, 1, j, n, m, S, B);
     }
-    return B;
+    return B.first;
 }
