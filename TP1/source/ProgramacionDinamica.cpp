0bd53c4eb7235c99b1a6142e55b5f13570db0229
basmaxxx <tuvelaprende@gmail.com>
2026-04-11 18:19:29 -0300

    Update ProgramacionDinamica.cpp
    funcion PD nueva + comentarios

--- TP1/source/ProgramacionDinamica.cpp
+++ TP1/source/ProgramacionDinamica.cpp
@@ -117,37 +117,28 @@ std::pair<int, double> encontrarSeamPDRec(const std::vector<std::vector<double>>
         return res;
     }
     
-    //incrementamos la energia actual
-
-
+    //definimos un mejor para comparar
+    std::pair<int, double > min;
+    int mejor; //la posicion del minimo
+    min.second= std::numeric_limits<double>::infinity();
 
     if(j > 0 && j < m-1){    //si no estoy en un borde 
-        std::pair<int, double > min;
-
-        int mejor;
-        min.second= std::numeric_limits<double>::infinity();
+        
+        //ciclo por las opciones del caso del medio => {j-1, j, j+1}
         for(int k=j-1; k <= j+1; k++){
-            std::pair<int, double> aux= encontrarSeamPDRec(energia, i-1, k, n , m, memo);
+            std::pair<int, double> aux= encontrarSeamPDRec(energia, i-1, k, n , m, memo); //llamado rec con la posicion k
             if(min.second>aux.second){//comparo energias
-                min=aux;//igualo pair si hay que cambiar el minimo
+
+                //actualizo los valores en caso de un nuevo minimo
+                min=aux;
                 mejor=k;
                 }
         }
 
-        //pongo solamente en el first el elemento de donde vino
-        min.first= mejor;
-        min.second += energia[i][j];
-
-
-        //actualizo memo
-        memo[i][j]= min;
-        return memo[i][j];
     }
     else if(j==0){
-        std::pair<int, double> min;
-        int mejor;
-
-        min.second= std::numeric_limits<double>::infinity();
+        
+        //pos para iterar=> {j, j+1}
         for(int k=j; k <= j+1; k++){
             std::pair<int, double> aux= encontrarSeamPDRec(energia, i-1, k, n , m, memo);
             if(min.second>aux.second){
@@ -156,30 +147,33 @@ std::pair<int, double> encontrarSeamPDRec(const std::vector<std::vector<double>>
                 
             }
         }
-        min.first= mejor;
-        min.second += energia[i][j];
-        memo[i][j]= min;
-        return memo[i][j];
+        
     }
     else{
-        std::pair<int, double> min;
-        int mejor;
-        min.second= std::numeric_limits<double>::infinity();
+        //pos para iterar=> {j-1, j}
+
         for(int k=j-1; k <= j; k++){
-            std::pair<int, double> aux = encontrarSeamPDRec(energia, i-1, k, n , m, memo);
+            std::pair<int, double> aux = encontrarSeamPDRec(energia, i-1, k, n , m, memo); 
             if(min.second>aux.second){
                 min=aux;
                 mejor= k;
             }
         }
+        
+    }
+    //pongo solamente en el first la posicion del elemento de donde vino
         min.first= mejor;
-        min.second += energia[i][j];
+        min.second += energia[i][j]; //sumo la energia acumulada
+
+
+        //actualizo memo
         memo[i][j]= min;
         return memo[i][j];
-    }
     
 }
 
+//funcion para invertir la solucion dado que en la reconstruccion queda como primer posicion el ultimo elemento del camino
+//devuelve el vector en orden=>primer elemento el primer elemento del camino
 std::vector<std::pair<int,int>> invertirPairs(std::vector<std::pair<int,int>> aInvertir){
     std::vector<std::pair<int,int>> res;
     for(int i=aInvertir.size()-1; i>=0; i--){
@@ -188,36 +182,38 @@ std::vector<std::pair<int,int>> invertirPairs(std::vector<std::pair<int,int>> aI
     return res;
 }
 
-std::vector<std::pair<int,int>> reconstruccion(const std::vector<std::vector<double>>& energia,  
-    std::vector<std::vector<std::pair<int, double>>> &memo, int colInicio){
-    int i= energia.size()-1;
-    int j= colInicio;
+
+//funcion que arma un vector(queda invertido) del camino de la costura optima(de menor energia)
+std::vector<std::pair<int,int>> reconstruccion(const std::vector<std::vector<double>>& energia, std::vector<std::vector<std::pair<int, double>>> &memo, int colInicio){
+    
+    int i= energia.size()-1;//empiezo en la ult fila
+    int j= colInicio;//columna pasada por parametro de que tiene menor energia acumulada
     std::vector<std::pair<int, int>> respuesta; //vector con las <i,j>
     respuesta.push_back({i,colInicio});//pusheo el ult elemento, obs: el vector va a estar invertido
     i--;//reduzco la iteracion
-    while(i > 0){
+    while(i >= 0){
         int posAnterior= memo[i][j].first;// valor de la pos del elemento anterior
         respuesta.push_back({i-1, posAnterior});
-        j = posAnterior;
+        j = posAnterior;//actualizo la columna en donde estoy
         i--;
     }
 
-    respuesta.push_back({0, j});
-
+    //llamo a la funcion que invierte la solucion, asi se devuelve el vector en el orden esperado
     std::vector<std::pair<int,int>> res= invertirPairs(respuesta);
     return res;
 
 }
 
 std::vector<std::pair<int,int>> encontrarSeamPDNueva(const std::vector<std::vector<double>>& energia) {
-    //creo la estructura de datos-->MEMO
+    //creo el memo
     int n= energia.size();
     int m= energia[0].size();
+    //memo= vector de vectores pair<int: columna "de donde viene", double: energia acumulada>
     std::vector<std::vector<std::pair<int, double>>> memo(
     n,
     std::vector<std::pair<int, double>>(m, {-1, std::numeric_limits<double>::infinity()}));
 
-
+    //se hace un llamado de la auxiliar por columna
     for(int j = 0; j < m; j++){
         encontrarSeamPDRec(energia, n-1, j, n, m, memo);
     }   
@@ -239,6 +235,7 @@ std::vector<std::pair<int,int>> encontrarSeamPDNueva(const std::vector<std::vect
         }
     }
 
+    //hago para la reconstruccion
     std::vector<std::pair<int, int>> posColumna= reconstruccion(energia, memo, pos);
     return posColumna;
 }
